5a78b13bded8b44b54b95afd68d909dbaaf0914b
dotuananh23030 <79571590+dotuananh23030@users.noreply.github.com>
2021-03-22 14:59:24 +0700

    Add files via upload

---
+++ Java/Assignment 1/src/Exam.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Exam {
+	int id;
+	String code;
+	String title;
+	CategoryQuestion[] categoryQuestion;
+	Duration duration;
+	Account creator;
+	Date createDate;
+	Question[] questions;
+
+}
---
+++ Java/Assignment 1/src/Group.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Group {
+	 int id;
+	 String name;
+     Account[] accounts;
+     Date createDate;
+
+}
